fcc9f192f35b55bca554a51a870a18ecabf0013f
titapo <suranyiabel@gmail.com>
2015-11-21 17:21:48 +0100

    FileHandler added with file read capability

---
+++ file_handler.js
@@ -0,0 +1,47 @@
+
+
+function FileHandler(filename)
+{
+    this.filename = filename;
+    var readyCallback = null;
+    this.setCallback = function(cb)
+    {
+        console.log("set + " + typeof(cb));
+        readyCallback = cb;
+        console.log("set ++" + typeof(readyCallback));
+    }
+
+    this.read = function()
+    {
+        var client = new XMLHttpRequest();
+        client.open('GET', this.filename);
+        var handlerRef = this;
+        client.onreadystatechange = function(handlerRef)
+        {
+            console.log("state:" + client.readyState);
+            /*
+            console.log("type:" + client.responseType);
+            console.log("text:" + client.responseText);
+            */
+            if (client.readyState !== 4) // not DONE
+                return;
+
+            if (readyCallback === null)
+            {
+                console.log("FileHandler() readyCallback is not set");
+                return;
+            }
+
+            readyCallback(client.responseText);
+        }
+        try
+        {
+        client.send();
+        }
+        catch (ex)
+        {
+            console.log("error: " + ex);
+        }
+
+    }
+}
